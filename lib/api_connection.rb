b8c695b852efc95054900f42c9912391b5dd9392
github username <github email address>
2018-12-06 09:11:08 -0500

    updated api connection file

--- lib/api_connection.rb
+++ lib/api_connection.rb
@@ -1,7 +1,7 @@
 #establish_connection to api here.
 
   def get_gifs_from_api
-    api_data = RestClient.get('https://api.giphy.com/v1/gifs/search?api_key=NC8kOxUoGiQZ5cor2YJV8MLrm1vlxLWY&q=cats&limit=25&offset=0&rating=PG&lang=en')
+    api_data = RestClient.get('https://api.giphy.com/v1/gifs/search?api_key=NC8kOxUoGiQZ5cor2YJV8MLrm1vlxLWY&q=cats&limit=100&offset=0&rating=PG&lang=en')
     JSON.parse(api_data)
   end
 
@@ -21,7 +21,7 @@
   end
 
   def keywords
-    get_keywords.flatten
+    get_keywords.flatten.uniq
   end
 
 
